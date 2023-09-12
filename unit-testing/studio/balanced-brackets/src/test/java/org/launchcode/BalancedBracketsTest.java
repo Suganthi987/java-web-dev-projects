05a4a0393ae7fec8f52961f113d8d4e3c5ea52c8
Suganthi <suganthisomalinga@gmail.com>
2023-09-11 20:32:14 -0500

    Final

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,52 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void singleBracketReturnsFalse() {
+        boolean condition = BalancedBrackets.hasBalancedBrackets("[");
+        assertFalse(condition);
+    }
+
+    @Test
+    public void emptyStringReturnsFalse() {
+        boolean condition = BalancedBrackets.hasBalancedBrackets("");
+        assertFalse(condition);
+    }
+
+    @Test
+public void isANumber(){
+        assertThrows(IllegalArgumentException.class, () -> BalancedBrackets.hasBalancedBrackets("5"),"String must include letters,not numbers");
+    }
+    @Test
+    public void unevenBrackets(){
+        boolean condition = BalancedBrackets.hasBalancedBrackets("[[]");
+        assertFalse(condition);
+    }
+
+    @Test
+    public void stringWithinBrackets(){
+        boolean condition = BalancedBrackets.hasBalancedBrackets("[string]");
+        assertTrue(condition);
+    }
+
+    @Test
+    public void stringWithCurlyBraces(){
+        boolean condition = BalancedBrackets.hasBalancedBrackets("}");
+        assertFalse(condition);
+    }
+
+    @Test
+    public void reverseBracket(){
+        boolean condition =  BalancedBrackets.hasBalancedBrackets("][");
+        assertFalse(condition);
+    }
+
+
 }
+
